432726bb088d7386f3f0166c9dcd5c4a847356fe
igolovatov <igolovatov@gmail.com>
2025-08-29 00:42:52 +0700

    некоторые доработки......

--- project/users/views.py
+++ project/users/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
+from django.views.generic import RedirectView
 from django.views.generic.edit import CreateView
 
 from .forms import CustomUserCreationForm
@@ -7,3 +9,10 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+class HomePageView(RedirectView):
+    template_name = 'home.html'
+
+    def get_redirect_url(self):
+        return reverse_lazy('home')
+
